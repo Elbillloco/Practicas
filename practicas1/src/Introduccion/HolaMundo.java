152fac0810fe84bf5f9e021bac8361a9e9a2e22a
Murillo <154952296+Murillo622@users.noreply.github.com>
2025-03-09 23:01:39 -0700

    Update HolaMundo.java

--- practicas1/src/Introduccion/HolaMundo.java
+++ practicas1/src/Introduccion/HolaMundo.java
@@ -2,6 +2,24 @@ package Introduccion;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("hola wey");
+        String nombre = "Hola mundo desde java";
+        System.out.println("saludar");
+        System.out.println("nombre.toUpperCase() = " + nombre.toUpperCase());
+        System.out.println("holamundo");
+
+        int numero = 10;
+
+
+        boolean valor = true;
+
+        int numero2 = 5;
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero4 = 10;
+        }
+        System.out.println("numero2= " + numero2);
+
+        var numero3 = "15";
+
     }
 }
